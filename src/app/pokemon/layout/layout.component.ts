3c66620452c04c73ba95e6e7edf2fabb15e87a1f
emilioCode <emilio_mem@hotmail.com>
2021-09-10 09:34:45 -0400

    tabs component created

--- src/app/pokemon/layout/layout.component.ts
+++ src/app/pokemon/layout/layout.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+type Tabs = 'tab-favorites' | 'tab-pokemons'; //with 'type' datatype option 1
+// enum Tabs{ //option 2
+//   tabfavorites,
+//   tabPokemons
+// }
+
 
 @Component({
   selector: 'pokemon-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
 
-  constructor() { }
+  
+  public currentTab: Tabs  = 'tab-pokemons' //?tab-favorites | tab-pokemons (option 1)
+  // public currentTab : Tabs = Tabs.tabPokemons; // with enum (option 2)
 
-  ngOnInit(): void {
-  }
-
+  constructor() { console.log(this.currentTab) }
 }
