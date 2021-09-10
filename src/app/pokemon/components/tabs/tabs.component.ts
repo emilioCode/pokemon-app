cd38fed5746f6dd04ff068460d43eaec1ff1b264
emilioCode <emilio_mem@hotmail.com>
2021-09-10 09:40:53 -0400

    tabs component created and implemented parcially

--- src/app/pokemon/components/tabs/tabs.component.ts
+++ src/app/pokemon/components/tabs/tabs.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+type Tabs = 'tab-favorites' | 'tab-pokemons'; //with 'type' datatype option 1
+// enum Tabs{ //option 2
+//   tabfavorites,
+//   tabPokemons
+// }
 
 @Component({
-  selector: 'app-tabs',
+  selector: 'pokemon-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent {
+
+  public currentTab: Tabs  = 'tab-pokemons' //?tab-favorites | tab-pokemons (option 1)
+  // public currentTab : Tabs = Tabs.tabPokemons; // with enum (option 2)
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
--- src/app/pokemon/layout/layout.component.ts
+++ src/app/pokemon/layout/layout.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 
-type Tabs = 'tab-favorites' | 'tab-pokemons'; //with 'type' datatype option 1
-// enum Tabs{ //option 2
-//   tabfavorites,
-//   tabPokemons
-// }
-
-
 @Component({
   selector: 'pokemon-layout',
   templateUrl: './layout.component.html',
@@ -14,9 +7,4 @@ type Tabs = 'tab-favorites' | 'tab-pokemons'; //with 'type' datatype option 1
 })
 export class LayoutComponent {
 
-  
-  public currentTab: Tabs  = 'tab-pokemons' //?tab-favorites | tab-pokemons (option 1)
-  // public currentTab : Tabs = Tabs.tabPokemons; // with enum (option 2)
-
-  constructor() { console.log(this.currentTab) }
 }
